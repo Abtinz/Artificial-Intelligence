449340b7870ff9433a4c415e3eb7f1630adb1aa2
Abtinz <abtinzandi@gmail.com>
2023-03-28 22:51:50 +0430

    DFSUnInformedSearchOfGraph

--- Uninformed Seacrch/DFS/DFS.py
+++ Uninformed Seacrch/DFS/DFS.py
@@ -5,6 +5,7 @@ def DFS(currentNode , graph , visitedNodesIndexes):
         for node in graph[currentNode]:
            DFS(node , graph , visitedNodesIndexes)
 
+#complete Graph search
 def DFSCompleteSearchOfGraph():
     graph = {
                 '6' : ['4','7'],
@@ -23,3 +24,46 @@ def DFSCompleteSearchOfGraph():
     print(visitedNodesIndexes)
 
 
+def DFS_with_goal(currentNode , graph , visitedNodesIndexes,goal):
+    if currentNode == goal: 
+
+        return 1
+    
+    else :
+        if len(graph[currentNode]) == 0 : 
+            return 0
+        if not currentNode in visitedNodesIndexes:
+            print(f'visiting {currentNode} node')
+            visitedNodesIndexes.append(currentNode)
+            index = 0
+            for node in graph[currentNode]:
+                index -=-1
+                if(DFS_with_goal(node , graph , visitedNodesIndexes,goal)):
+                    return 1
+                else:
+                    if index == len(graph[currentNode]):
+                        visitedNodesIndexes.pop()
+                        return 0
+
+
+def DFSUnInformedSearchOfGraph():
+    graph = {
+                '6' : ['2','4'],
+                '5' : ['0','7'],
+                '18' : ['22'],
+                '22' : ['5','18'],
+                '0' : ['5', '6' , '7'],
+                '7' : ['8'],
+                '2' : ['12'],
+                '12' : ['66'],
+                '66' : [],
+                '4' : ['8'],
+                '8' : ['22']
+    }
+
+    goal = '7'
+    visitedNodesIndexes = []
+    DFS_with_goal('6' , graph , visitedNodesIndexes,goal)
+    print(f"the best way to reach into {goal} is {visitedNodesIndexes}")
+
+DFSUnInformedSearchOfGraph()
